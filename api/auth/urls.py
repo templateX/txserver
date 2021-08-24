3cd94c0bb60590e8f8431981d47ce43ff32ae569
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2021-08-24 08:03:09 +0530

    adding github callback

--- api/auth/urls.py
+++ api/auth/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
+    path('github/login/', views.github_login, name='github_login'),
     path('github/login/callback/', views.github_callback, name='github_callback')
 ]
--- api/auth/views.py
+++ api/auth/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
+
+def github_login(request):
+    url = 'https://github.com/login/oauth/authorize'
+    return redirect(url)
 
 def github_callback(request):
-    print(request.GET)
+    print(request.GET['code'])
     return HttpResponse('Done')
