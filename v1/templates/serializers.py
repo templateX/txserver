ab21c0f95796f319deb9974fb3c3f269fc7b56a0
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2021-08-28 00:21:24 +0530

    added tags and repos urls

--- v1/templates/serializers.py
+++ v1/templates/serializers.py
@@ -1,7 +1,9 @@
+from re import template
 from rest_framework import serializers
 from templates.models import Template, Repo
 from tags.models import Tag
 from accounts.models import User
+
 
 class TagSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField()
@@ -10,15 +12,18 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ('id', 'name')
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'name', 'email')
 
+
 class RepoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Repo
         fields = ('id', 'provider', 'link')
+
 
 class TemplateSearchSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True)
@@ -28,20 +33,29 @@ class TemplateSearchSerializer(serializers.ModelSerializer):
         model = Template
         fields = ('title', 'tags', 'like_count')
 
+
 class TemplateCreateSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     tags = TagSerializer(many=True, read_only=False)
+    repos = RepoSerializer(many=True, read_only=False)
 
     class Meta:
         model = Template
-        fields = ('id', 'user', 'title', 'description', 'slug', 'preview_link', 'tags', 'created_at', 'updated_at')
+        fields = ('id', 'user', 'title', 'description', 'slug', 'preview_link', 'tags', 'repos', 'created_at', 'updated_at')
 
     def create(self, validated_data):
         tags = validated_data.pop('tags')
+        repos = validated_data.pop('repos')
+
         instance = super().create(validated_data)
         for tag in tags:
             instance.tags.add(Tag.objects.get(**tag))
+
+        for repo in repos:
+            Repo.objects.create(template=instance, **repo)
+
         return instance
+
 
 class TemplateSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
--- v1/templates/views.py
+++ v1/templates/views.py
@@ -1,7 +1,11 @@
-from rest_framework import generics, permissions
-from .serializers import TemplateSearchSerializer, TemplateSerializer, TemplateCreateSerializer
-from .permissions import IsOwnerOrReadOnly
-from templates.models import Template
+from rest_framework import generics, permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import TemplateSearchSerializer, TemplateSerializer, TemplateCreateSerializer, RepoSerializer
+from .permissions import IsOwner, IsOwnerOrReadOnly
+from .exceptions import TemplateUnavailable, TagUnavailable, RepoUnavailable, InvalidData
+from templates.models import Template, Tag, Repo
+from v1.response import Success, SuccessCreate, SuccessUpdate, SuccessDelete, InvalidPermission
 
 
 class TemplateSearch(generics.ListAPIView):
@@ -30,3 +34,96 @@ class TemplateDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TemplateSerializer
     queryset = Template.objects.all()
     permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly)
+
+
+class LikeView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, pk):
+        try:
+            template = Template.objects.get(pk=pk)
+            template.likes.add(request.user)
+            raise Success
+        except Template.DoesNotExist:
+            raise TemplateUnavailable
+
+
+class LikeRemoveView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, pk):
+        try:
+            template = Template.objects.get(pk=pk)
+            template.likes.remove(request.user)
+            raise Success
+        except Template.DoesNotExist:
+            raise TemplateUnavailable
+
+
+class TemplateTag(APIView):
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+    def get_object(self, pk):
+        try:
+            template = Template.objects.get(pk=pk)
+            return template
+        except Template.DoesNotExist:
+            raise TemplateUnavailable
+
+    def get_tag(self, tag_id):
+        try:
+            tag = Tag.objects.get(pk=tag_id)
+            return tag
+        except Tag.DoesNotExist:
+            raise TagUnavailable
+
+    def get(self, request, pk):
+        template = self.get_object(pk)
+        self.check_object_permissions(self.request, template)
+        tag = self.get_tag(request.query_params['tag_id'])
+        template.tags.add(tag)
+        raise Success
+
+    def delete(self, request, pk):
+        template = self.get_object(pk)
+        self.check_object_permissions(request, template)
+        tag = self.get_tag(request.query_params['tag_id'])
+        template.tags.remove(tag)
+        raise Success
+
+
+class TemplateRepo(APIView):
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+    def get_object(self, pk):
+        try:
+            template = Template.objects.get(pk=pk)
+            return template
+        except Template.DoesNotExist:
+            raise TemplateUnavailable
+
+    def get_repo(self, repo_id):
+        try:
+            repo = Repo.objects.get(pk=repo_id)
+            return repo
+        except Tag.DoesNotExist:
+            raise RepoUnavailable
+
+    def post(self, request, pk):
+        template = self.get_object(pk)
+        self.check_object_permissions(request, template)
+        serializer = RepoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(template=template)
+        else:
+            raise InvalidData
+        raise SuccessCreate
+
+    def delete(self, request, pk):
+        template = self.get_object(pk)
+        self.check_object_permissions(request, template)
+        repo = self.get_repo(request.query_params['repo_id'])
+        if not repo.template == template:
+            raise InvalidPermission
+        repo.delete()
+        raise SuccessDelete
